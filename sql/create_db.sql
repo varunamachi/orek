f5dd77f0fb09f02478e76440dbd5049bf0fbf050
varunamachi <varunamachi@gmail.com>
2015-12-31 19:00:06 +0530

    SQL queries work now on MySql 5.6.25

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -6,9 +6,9 @@ CREATE TABLE orek_user(
     user_name     VARCHAR( 255 ) NOT NULL,
     first_name    VARCHAR( 255 ),
     last_name     VARCHAR( 255 ),
-    emai          VARCHAR( 255 ) NOT NULL,
-    UNIQUE( email )
+    email          VARCHAR( 255 ) NOT NULL,
     PRIMARY KEY( user_name ),
+    UNIQUE( email )
 );
 
 CREATE TABLE orek_user_identity(
@@ -17,9 +17,9 @@ CREATE TABLE orek_user_identity(
     digest      VARCHAR( 255 ) NOT NULL,
     PRIMARY KEY( user_name ),
     UNIQUE( email ),
-    FOREIGN KEY user_name REFERENCES orek_user( user_name ),
-    FOREIGN KEY email REFERENCES orek_user( email )
-)
+    FOREIGN KEY( user_name ) REFERENCES orek_user( user_name ),
+    FOREIGN KEY( email ) REFERENCES orek_user( email )
+);
 
 
 --'access' is an enum -> private, group, public
@@ -36,11 +36,13 @@ CREATE TABLE orek_source(
 
 
 CREATE TABLE orek_variable(
+    variable_id  CHAR( 36 )     NOT NULL,
     name         VARCHAR( 255 ) NOT NULL,
     source_id    CHAR( 36 )     NOT NULL,
     description  TEXT           NOT NULL,
     unit         CHAR( 30 )     NOT NULL,
-    PRIMARY KEY( source_id, name ),
+    PRIMARY KEY( variable_id ),
+    UNIQUE( source_id, name ),
     FOREIGN KEY( source_id ) REFERENCES orek_source( source_id )
     
 );
@@ -51,14 +53,44 @@ CREATE TABLE orek_user_group(
     owner       VARCHAR( 256 ) NOT NULL,
     description TEXT NOT NULL,
     PRIMARY KEY( name ),
-    FOREIGN KEY owner REFERENCES orek_user( user_name )
+    FOREIGN KEY( owner ) REFERENCES orek_user( user_name )
 );
 
 CREATE TABLE orek_user_to_group(
     group_name  VARCHAR( 256 ) NOT NULL,
     user_name   VARCHAR( 256 ) NOT NULL,
-    FOREIGN KEY group_name REFERENCES orek_user_group( name ),
-    FOREIGN KEY user_name REFERENCES orek_user( user_name ),
-)
+    FOREIGN KEY( group_name ) REFERENCES orek_user_group( name ),
+    FOREIGN KEY( user_name ) REFERENCES orek_user( user_name ),
+    PRIMARY KEY( group_name, user_name )
+);
 
 
+CREATE TABLE orek_variable_group(
+    group_id    CHAR( 36 )     NOT NULL,
+    name        VARCHAR( 256 ) NOT NULL,
+    owner       VARCHAR( 256 ) NOT NULL,
+    description TEXT           NOT NULL,
+    access      CHAR( 20 )     NOT NULL DEFAULT "public",
+    PRIMARY KEY( group_id ),
+    UNIQUE( name, owner ),
+    FOREIGN KEY( owner ) REFERENCES orek_user( user_name )
+);
+
+
+CREATE TABLE orek_variable_to_group(
+    var_group_id        CHAR( 36 ) NOT NULL,    
+    variable_id         CHAR( 36 ) NOT NULL,
+    PRIMARY KEY( var_group_id, variable_id ),
+    FOREIGN KEY( var_group_id ) REFERENCES orek_variable_group( group_id ),
+    FOREIGN KEY( variable_id ) REFERENCES orek_variable( variable_id )
+);
+
+
+CREATE TABLE orek_variable_value(
+    variable_id         CHAR( 36 ) NOT NULL,
+    value               VARCHAR( 256 ) NOT NULL,
+    time                TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_orek_var_value ON orek_variable_value( variable_id );
+ 
